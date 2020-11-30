3e01bebcb06f8545398e1ec088bf086e09d15652
Ara <hello.ara@pm.me>
2020-12-01 02:58:45 +0900

    midway step 11

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,16 +1,37 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey, :entry_station
 
-  MAX_BALANCE = 90
+
   DEFAULT_BALANCE = 0
+  MAX_BALANCE = 90
+  MINIMUM_BALANCE = 1
+  FARE = 2
 
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
+    @in_journey = false
   end
 
   def top_up(money)
-    fail 'top-up limit exceeded' if money + balance > MAX_BALANCE
+    fail "£#{MAX_BALANCE} max. value exceeded" if money + balance > MAX_BALANCE
     @balance += money
+  end
+
+  def touch_in(station)
+    fail "Below £#{Oystercard::MINIMUM_BALANCE} minimum balance." if balance < MINIMUM_BALANCE
+    @in_journey = true
+    @entry_station = station
+  end
+
+  def touch_out
+    deduct
+    @in_journey = false
+  end
+
+  private
+
+  def deduct
+    @balance -= FARE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,12 +21,49 @@ describe Oystercard do
     #   expect { card.top_up(90) }.to raise_error "top-up limit exceeded"
     # end
     it 'raises an error when top_up exceeds limit' do
-      allow(card).to receive(:top_up).and_return(50)
+      # allow(card).to receive(:top_up).and_return(50)
       card.top_up(50)
-      p card.balance 
-      expect { card.top_up(41) }.to raise_error "top-up limit exceeded"
+      expect { card.top_up(41) }.to raise_error "£#{Oystercard::MAX_BALANCE} max. value exceeded"
     end
   end
 
+  # describe '#deduct' do
+  #   it 'deducts a fare from the oyster card' do
+  #     expect(card).to respond_to(:deduct)
+  #     expect{ card.deduct }.to change { card.balance }
+  #   end
+  # end
+
+  describe '#touch_in' do
+    it 'updates the in_journey variable' do
+      card.top_up(10)
+      card.touch_in("Holborn")
+      expect(card.in_journey).to be true
+    end
+
+    it 'refuses entry if balance is below minimum balance' do
+      card.top_up(0.5)
+      expect {card.touch_in("Holborn")}.to raise_error "Below £#{Oystercard::MINIMUM_BALANCE} minimum balance."
+    end
+
+    it 'remembers the entry station' do
+      card.top_up(10)
+      card.touch_in("Holborn")
+      expect(card.entry_station).to eq "Holborn"
+    end
+
+  end
+
+  describe '#touch_out' do
+    it 'updates the in_journey' do
+      card.touch_out
+      expect(card.in_journey).to be false
+    end
+
+    it 'reduces the balance by the amount of fare' do
+      card.top_up(10)
+      expect {card.touch_out}.to change {card.balance}.from(10).to(8)
+    end
+  end
 
 end
